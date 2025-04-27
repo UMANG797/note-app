c6b3aa3200a00c4b5405356bd45d1e22c8c547a8
UMANG797 <umangrathod797@gmail.com>
2025-04-27 10:20:15 +0530

    first-change

--- frontend/src/components/Input/PasswordInput.jsx
+++ frontend/src/components/Input/PasswordInput.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import { FaRegEye } from "react-icons/fa6";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
 
 const PasswordInput = ({ value, onChange, placeholder }) => {
   const [isShowPassword, setIsShowPassword] = useState(false);
 
   const toggleShowPassword = () => {
-    setIsShowPassword((prev) => !prev);
+    setIsShowPassword(!isShowPassword);
   };
 
   return (
@@ -15,13 +15,21 @@ const PasswordInput = ({ value, onChange, placeholder }) => {
         onChange={onChange}
         type={isShowPassword ? "text" : "password"}
         placeholder={placeholder || "Password"}
-        className="w-full text-sm bg-transparent py-3 mr-3 rounded outline-none"
+        className="w-full text-sm bg-transparent py-1 mr-3  outline-none"
       />
-      <FaRegEye
-        size={22}
-        className="text-primary cursor-pointer ml-2"
-        onClick={toggleShowPassword}
-      />
+      {isShowPassword ? (
+        <FaRegEye
+          size={26}
+          className="text-primary cursor-pointer ml-2 text-blue-600"
+          onClick={toggleShowPassword}
+        />
+      ) : (
+        <FaRegEyeSlash
+          size={26}
+          className="text-primary cursor-pointer ml-2 text-blue-600"
+          onClick={toggleShowPassword}
+        />
+      )}
     </div>
   );
 };
--- frontend/src/pages/SignUp/SignUp.jsx
+++ frontend/src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,63 @@
-import React from "react";
+import { useState } from "react";
+import PasswordInput from "../../components/Input/PasswordInput";
+import Navbar from "../../components/Navbar/Navbar";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [passwod, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <Navbar />
+
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+          <form onSubmit={handleSignUp}>
+            <h4 className="text-2xl mb-7">SignUp</h4>
+            <input
+              type="text"
+              placeholder="Name"
+              className="input-box"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Email"
+              className="input-box"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <PasswordInput
+              value={passwod}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder={"Password"}
+            />
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+            <button type="submit" className="btn-primary">
+              SignUp
+            </button>
+            <p className="text-sm text-center mt-4">
+              Not registerd yet?{""}
+              <Link
+                to="/login"
+                className="font-medium  underline  text-blue-600 pl-2"
+              >
+                Login
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
