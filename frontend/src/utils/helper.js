d559f42a59163e32cbabca35307e39db1fcebbfb
UMANG797 <umangrathod797@gmail.com>
2025-04-27 11:22:13 +0530

    notes-dashboard-feature adding

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -2,4 +2,17 @@ export const validEmail = (email) => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
   };
-  
+
+  export const getInitials=(name)=>{
+    if(!name) return "";
+
+    const word=name.split(" ");
+    let initials="";
+
+    for(let i=0; i<Math.min(word.length,2); i++)
+    {
+      initials += word[i][0];
+    }
+
+    return initials.toUpperCase();
+  }
