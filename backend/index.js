7a8fe79fdcb4f527943d6c256e0f0e944b3efede
UMANG797 <umangrathod797@gmail.com>
2025-04-29 15:59:33 +0530

    fetching data work done completely

--- backend/index.js
+++ backend/index.js
@@ -135,7 +135,18 @@ app.post("/add-note", authenticateToken, async (req, res) => {
     }
 });
 
+app.get("/get-all-notes",authenticateToken,async(req,res)=>{
+    const {user}=req.user;
 
+    try{
+        const notes=await Note.find({userId:user._id}).sort({isPinned:-1});
+        return res.json({error:false,notes,message:"all notes fetch successfully"});
+    }
+    catch(err)
+    {
+        return res.json(500).json({error:true,mesage:"Internal server error"});
+    }
+})
 
 
 app.listen(8000, () => {
