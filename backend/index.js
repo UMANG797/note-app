5b6ab2ce5f6e8e8537815612129b569c2d6b0284
UMANG797 <umangrathod797@gmail.com>
2025-04-29 13:11:15 +0530

    authenticatation backend is done

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ const config=require("./config.json");
 const mongoose=require("mongoose");
 
 mongoose.connect(config.connectionString)
+.then(()=>{console.log("Mongodb is connected")})
+.catch((err)=>{console.log("something is being problem error is "+err)})
+
+const User=require('./models/user.model');
+
 const express=require("express");
 const cors=require("cors");
 const app=express();
@@ -22,8 +27,39 @@ app.post("/create-account",async(req,res)=>{
     const {fullName,email,password}=req.body;
 
     if(!fullName) return res.status(400).json({error:true,message:"FullName is required"});
+
+    if(!email) return res.status(400).json({error:true,message:"Email is required"});
+
+    if(!password) return res.status(400).json({error:true,message:"password is required"});
+
+    const isUser=await User.findOne({email:email});
+    if(isUser) {return res.json({error:true,message:"User already exist"})};
+
+    const user=new User({fullName,email,password});
+    await user.save();
+
+    const accessToken=jwt.sign({user},process.env.ACESS_TOKEN_SECRET,{expiresIn:"30m"})
+    return res.json({error:false,user,accessToken,message:"Registration successfull"});
 })
 
-app.listen(8000);
+app.post("/login",async(req,res)=>{
+    const {email,password}=req.body;
+    if(!email) return res.status(400).json({error:true,message:"Email is required"});
+    if(!password) return res.status(400).json({error:true,message:"Password is required"});
+    const userInfo=await User.findOne({email:email});
+    if(!userInfo) return res.status(401).json({error:true,message:"User not Found"});
+    if(userInfo.email == email && userInfo.password == password)
+    {
+        const user={user:userInfo};
+        const accessToken=jwt.sign(user,process.env.ACESS_TOKEN_SECRET,{expiresIn:"3600m"});
+        return res.json({error:false,message:"Login SuccessFull",email,accessToken});
+    }
+    else
+    {
+        return res.status(400).json({error:true,message:"Invalid creadentails"});
+    }
+})
+
+app.listen(8000,()=>{console.log("port is running")});
 
 module.exports=app;
