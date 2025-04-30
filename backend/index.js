4c1dce664dcf2d85d29adbe39d7d4934798fbd15
UMANG797 <umangrathod797@gmail.com>
2025-04-30 23:14:00 +0530

    updating pin to unpin and geting user information in backend is done so almost the backend is over

--- backend/index.js
+++ backend/index.js
@@ -166,8 +166,47 @@ app.delete("/delete-note/:noteId",authenticateToken,async(req,res)=>{
     {
         return res.status(400).json({error:true,message:"Internal server error"});
     }
+});
+
+app.put("/update-note-pinned/:noteId",authenticateToken,async(req,res)=>{
+  const noteId=req.params.noteId;
+  const {isPinned}=req.body;
+  const {user}=req.user;
+
+  if(!isPinned)
+  {
+    return res.status(400).json({error:true,message:"No changes provided"});
+  }
+  try
+  {
+    const note=await Note.findOne({_id:noteId,userId:user._id});
+
+    if(!note)
+    {
+      return res.status(404).json({error:true,message:"Note not found"});
+    }
+  if(isPinned) note.isPinned=isPinned || false;
+
+  await note.save();
+
+  return res.json({error:false,note,message:"Note updated successfully"});
+  }
+  catch(err)
+  {
+    return res.status(500).json({error:true,message:"Internal server error "+err});
+  }
 })
 
+app.get("/get-user",authenticateToken,async(req,res)=>{
+  const {user}=req.user;
+
+  const isUser=await User.findOne({_id:user._id});
+  if(!isUser)
+  {
+    return res.sendStatus(401);
+  }
+  return res.json({fullName:isUser.fullName,email:isUser.email,"_id":isUser._id,createdOn:isUser.createdOn})
+})
 app.listen(8000, () => {
   console.log("Server running on port 8000");
 });
